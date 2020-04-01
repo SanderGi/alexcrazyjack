f0ea1c92f31de5568494a80bb7ce27e0f355a027
Glitch (hello-express) <none>
2020-04-01 04:36:17 +0000

    🚌⛱ Checkpoint ./game.js:12528664/348

--- game.js
+++ game.js
@@ -1,6 +1,15 @@
 class game {
-  constructor() {
+  constructor(host) {
     this.status = "requested";
-    this.
+    var date = new Date(); 
+    this.started = date.getTime();
+    this.updated = this.started;
+    
+    this.player1 = null;
+    this.player2 = host;
+    this.lastMoveBy = 2;
+    
+    this.moves = [];
+    this.longestMoveTime = 0;
   }
 }
