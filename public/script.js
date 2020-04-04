20bcd2a43260eced9b27a4977d35f82144b8532d
Alexander Metzger <alexander.le@outlook.dk>
2020-04-04 18:43:17 +0000

    👚🚄 Checkpoint ./public/index.html:12528664/531 ./public/script.js:12528664/2990

--- public/script.js
+++ public/script.js
@@ -2,6 +2,23 @@
 //   console.log(await res.json());
 // });
 
-function DisplayBoard() {
-  
+CreateBoard();
+function resizeBoard() {
+  let width = (window.innerHeight > window.innerWidth) ? "100%" : "calc(100vh - 14em)";
+  document.getElementById("board").style = "width: " + width + "; height: calc(100vh - 14em); margin: auto; border-left: dashed; border-right: dashed";
+  document.getElementById("name").innerHTML = width;
+}
+resizeBoard()
+window.onresize = function() { resizeBoard(); };
+
+function CreateBoard() {
+  let board = document.getElementById("board");
+  let markup = "";
+  markup += createCardMarkup("big", "joker", "left");
+  board.innerHTML = markup;
+}
+
+function createCardMarkup(rank, suit, float) {
+  if (suit == "joker") return '<div style="width: 10%; height: 10%; overflow: hidden; margin: 0; padding: 0; float: '+float+'"><div class="card joker '+suit+'"><span class="rank"></span><span class="suit">Joker</span></div></div>';
+  else return "<div style='width: 10%; height: 10%; overflow: hidden; margin: 0; padding: 0; float: " + float + "><div class='card rank-" + rank + " " + suit + "'><span class='rank'>"+rank+"</span><span class='suit'>&"+suit+";</span></div></div>";
 }
