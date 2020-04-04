26622692140fd572f3a74266ea0a85246d5617b9
Alexander Metzger <alexander.le@outlook.dk>
2020-04-04 22:13:22 +0000

    🏩🛋 Checkpoint ./public/script.js:12528664/1719 ./public/index.html:12528664/102

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ let index = 0;
 CreateBoard();
 function resizeBoard() {
   let width = (window.innerHeight > window.innerWidth) ? "100%" : "calc(100vh - 14em)";
-  document.getElementById("board").style = "width: " + width + "; height: calc(100vh - 14em); margin: auto;" + (width === "100%" ? "border: none" : "border-left: solid; border-right: solid;");
+  document.getElementById("board").style = "width: " + width + "; height: calc(100vh - 14em); margin: auto; " + (width === "100%" ? "border: none" : "border-left: solid; border-right: solid;");
   // document.getElementById("name").innerHTML = width;
   if (window.innerHeight > window.innerWidth) document.getElementById("cardCanvas").className = "playingCards " + (window.innerWidth > 200 ? "faceImages" : "simpleCards");
   else document.getElementById("cardCanvas").className = "playingCards " + (window.windowHeight > 200 ? "faceImages" : "simpleCards");
@@ -14,7 +14,9 @@ function resizeBoard() {
 resizeBoard()
 window.onresize = function() { resizeBoard(); };
 
-function MakeMove(rank, suit, col, row) {
+function MakeMove(rank, suit, index) {
+  let col = index % 10;
+  let row = Math.floor(index / 10);
   document.getElementById("name").innerHTML = rank + " " + suit + " " + col + " " + row;
 }
 
@@ -93,15 +95,27 @@ function CreateBoard() {
   board.appendChild(createCard("Q", "clubs"));
   board.appendChild(createCard(6, "clubs"));
   board.appendChild(createCard(9, "clubs"));
+  //
+  board.appendChild(createCard(10, "hearts"));
+  board.appendChild(createCard("K", "hearts"));
+  for (let i = 6; i >= 4; i--) board.appendChild(createCard(i, "spades"));
+  for (let i = 4; i <= 6; i++) board.appendChild(createCard(i, "diams"));
+  board.appendChild(createCard("K", "clubs"));
+  board.appendChild(createCard(10, "clubs"));
+  //
+  board.appendChild(createCard("", "joker"));
+  for (let i = 10; i >= 7; i--) board.appendChild(createCard(i, "spades"));
+  for (let i = 7; i <= 10; i++) board.appendChild(createCard(i, "diams"));
+  board.appendChild(createCard("big", "joker"));
 }
 
 function createCard(rank, suit) {
   let card = document.createElement('div');
-  card.style = "width: 10%; height: 10%; overflow: hidden; margin: 0; padding: 0; float: left";
+  card.style = "width: 10%; height: calc((100vh - 14em) / 10); overflow: hidden; margin: 0; padding: 0; float: left";
   if (suit == "joker") card.innerHTML = '<div class="card joker '+rank+'"><span class="rank"></span><span class="suit">Joker</span></div>';
   else card.innerHTML = '<div class="card rank-' + rank.toString().toLowerCase() + ' ' + suit + '"><span class="rank">'+rank+'</span><span class="suit">&'+suit+';</span></div>';
-  card.data
-  card.addEventListener('touchstart', (e) => MakeMove(rank, suit, e.target.dataset.index));
+  card.dataset.index = index;
+  card.addEventListener('touchstart', (e) => { MakeMove(rank, suit, card.dataset.index); });
   index++;
   return card;
   // if (suit == "joker") return '<div style="width: 10%; height: 10%; overflow: hidden; margin: 0; padding: 0; float: left"><div class="card joker '+rank+'"><span class="rank"></span><span class="suit">Joker</span></div></div>';
