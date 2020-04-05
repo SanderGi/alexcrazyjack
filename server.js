3ba847e443ef5e3358c4a1b26a5008357b2e883c
Alexander Metzger <alexander.le@outlook.dk>
2020-04-05 17:43:20 +0000

    🚦🍉 Checkpoint ./server.js:12528664/2934

--- server.js
+++ server.js
@@ -27,7 +27,11 @@ app.get("/", (request, response) => {
 });
 
 app.get("/sendMove", function (request, response) {
-    response.sendStatus(200);
+    let id = request.query.id;
+    let move = request.query.move;
+    db.find({id: id}, function (err, games) { 
+      db.update({id: id}, {name : "gibson thunderbird", year: 1990}, {});
+    });
 });
 
 app.get("/getGame", function (request, response) {
@@ -41,27 +45,45 @@ app.get("/createGame", function (request, response) {
   let player2 = request.query.name;
   let date = new Date();
   let deck = createDeck(2);
-  deck = shuffle(deck);
+  shuffle(deck);
   let id = Date.now()
-  db.insert({id: id, status: "invite send", moves: ["-a0", "-a9", "-j0", "-j9"], player1: null, player2: player2, deck1: [], deck2: [], turn: 1, lastUpdated: id});
+  db.insert({id: id, status: "invite send", moves: ["-a0", "-a9", "-j0", "-j9"], player1: null, player2: player2, deck1: deck.splice(0,51), deck2: deck, turn: 1, lastUpdated: id});
   response.send(id);
 });
 
 app.get("/joinGame", function (request, response) {
     let id = request.query.id;
-    response.sendStatus(200);
+    db.find({id: id}, function (err, games) { 
+      if (games[0].status !== "invite send") response.send("GAME IS NOT JOINABLE");
+      else {
+        db.update(games[0], 
+            {id: id, status: "playing", moves: ["-a0", "-a9", "-j0", "-j9"], player1: null, player2: player2, deck1: deck.splice(0,51), deck2: deck, turn: 1, lastUpdated: id}, {}
+        );
+        response.send(games[0]);
+      }
+    });
 });
 
 const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
 const suits = ['H', 'D', 'S', 'C'];
 function createDeck(n) {
   let deck = [];
-  for (let i = 0; i < suits.length; i++) {
-    for (let j = 0; j < ranks.length; j++) {
-      deck.push(ranks[j] + suits[i]);
+  for (let k = 0; k < n; k++) {
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 0; j < ranks.length; j++) {
+        deck.push(ranks[j] + suits[i]);
+      }
     }
   }
+  
   return deck;
+}
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
 
 // listen for requests :)
